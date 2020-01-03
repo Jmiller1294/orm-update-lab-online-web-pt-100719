6de7b3215bc571c9db2caab67e3bd4246d7de6cd
Justin Miller <jayman1294@gmail.com>
2020-01-03 16:16:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,11 +51,7 @@ end
   end
   
   def self.new_from_db(row)
-    new_song = self.new  # self.new is the same as running Song.new
-  new_song.id = row[0]
-  new_song.name =  row[1]
-  new_song.length = row[2]
-  new_song
+   
   end
   
   def self.find_by_name(name)
