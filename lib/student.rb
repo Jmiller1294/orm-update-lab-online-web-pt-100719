f3abe4f7f22d69754ef2bef8a8e3f8593221465a
Justin Miller <jayman1294@gmail.com>
2020-01-03 14:56:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,11 +16,11 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students(
-      INTEGER id
-    
-    
-    
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
     )
+    SQL
   end
 
 
