c57b7a5e41c4bbe427e7e73bd172cce601f2d40e
Justin Miller <jayman1294@gmail.com>
2020-01-03 14:55:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,15 @@ class Student
     @grade = grade
   end
   
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS (
+      INTEGER id
+    
+    
+    
+    )
+  end
 
 
 end
