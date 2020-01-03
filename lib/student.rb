c1cc6da36993bcdf5a152ef3aeb27e9cc62ca988
Justin Miller <jayman1294@gmail.com>
2020-01-03 16:10:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,10 +50,11 @@ end
   end
   
   def self.new_from_db(row)
+    
     student.id = row[0]
     name = row[1]
     grade = row[2]
-    self.new(id, name, grade)
+    
   end
   
   def self.find_by_name(name)
